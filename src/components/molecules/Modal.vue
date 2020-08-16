433d814983064c49263c44daf90386055ee0fd40
佐橋周作 <13200387+Shusaku-Sahashi@users.noreply.github.com>
2020-08-16 23:46:22 +0900

    added: modal conponent

---
+++ src/components/molecules/Modal.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="example-modal-window">
+    <button v-on:click="openModal">show modal</button>
+    <modal-container v-on:close="closeModal" v-if="isDisplayModal">
+      <p>Vue.js Modal Window!</p>
+      <div>
+        <input v-model="message" placeholder="edit message ..."/>
+      </div>
+      <template slot="footer">
+        <button @click="doSend">送信</button>
+      </template>
+    </modal-container>
+  </div>
+</template>
+
+
+<script>
+import ModalContainer from "@/components/atoms/ModalContainer";
+export default {
+  name: "Modal",
+  data() {
+    return {
+      isDisplayModal: false,
+      message: ""
+    }
+  },
+  components: {ModalContainer},
+  methods: {
+    closeModal() {
+      this.isDisplayModal = false
+    },
+    openModal() {
+      this.isDisplayModal = true
+    },
+    doSend() {
+      if (this.message) {
+        alert(this.message)
+        this.message = ''
+        this.closeModal()
+      } else {
+        alert('メッセージを入力してください')
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/stories/index.js
+++ src/stories/index.js
@@ -1,7 +1,14 @@
 import { storiesOf } from '@storybook/vue';
-import CustomButton from "@/components/CustomButton";
+import CustomButton from "@/components/atoms/CustomButton";
+import Modal from "@/components/molecules/Modal";
 
 storiesOf('TaskLaneItem', module).add('Default TaskLaneItem', () => ({
   components: {CustomButton},
   template: `<custom-button>This is Test</custom-button>`
+}));
+
+
+storiesOf('Modal', module).add('Modal', () => ({
+  components: {Modal},
+  template: `<modal></modal>`
 }));
